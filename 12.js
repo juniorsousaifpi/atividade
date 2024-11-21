d0d5a23110dba9576a9df4c97671571603b02ad5
ian gabriel <ianzoka763@gmail.com>
2024-11-21 19:44:19 -0300

    first commit

---
+++ 12.js
@@ -0,0 +1,9 @@
+//solicitando a altura dop usuario em metro
+let alturastring = prompt("digite sua altura em metros (exemplo: 1.75):");
+
+//convertido a altyura para numero de ponto flutuante
+let alturafloat = 
+parseFloat(alturastring);
+
+//exibindop altura convertida no console
+console.log("sua altura convertida e:",alturafloat,"metros");
---
+++ 6.js
@@ -0,0 +1,9 @@
+let numeroInteiro = 10
+
+let numero_decimal = 3.14;
+
+
+console.log("tipo de 'numero_Inteiro':",typeof
+numeroInteiro);
+console.log("tipo de 'numero_decimal':",typeof
+numero_decimal);
